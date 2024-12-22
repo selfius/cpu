8429d7c72c9a21e36268a94486e087092af71b0b
Andrei Sviridov <self.polaris@gmail.com>
2024-12-22 13:34:27 -0500

    Implement AND gate

---
+++ src/and.rs
@@ -0,0 +1,47 @@
+use crate::nand::nand;
+use crate::not::not;
+use digital_component::{ComponentLogic, ComponentLogicFactory};
+use parser::parse;
+use std::collections::HashMap;
+
+pub fn and() -> Box<ComponentLogic> {
+    let mut functions: HashMap<&str, Box<ComponentLogicFactory>> = HashMap::new();
+    functions.insert("nand", Box::new(|| Box::new(nand)));
+    functions.insert("not", Box::new(not));
+    parse(
+        "
+        ┏━━━━┓ ┏━━━┓
+       ─┨nand┠─┨not┠─
+       ─┨    ┃ ┗━━━┛
+        ┗━━━━┛
+    ",
+        &functions,
+    )
+    .unwrap()
+    .finalize()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use digital_component::*;
+
+    #[test]
+    fn nots() {
+        let mut and_gate = and();
+
+        let mut output = vec![BitState::Undefined];
+
+        and_gate(&[BitState::Off, BitState::Off], &mut output);
+        assert_eq!(output, vec![BitState::Off]);
+
+        and_gate(&[BitState::On, BitState::Off], &mut output);
+        assert_eq!(output, vec![BitState::Off]);
+
+        and_gate(&[BitState::On, BitState::Off], &mut output);
+        assert_eq!(output, vec![BitState::Off]);
+
+        and_gate(&[BitState::On, BitState::On], &mut output);
+        assert_eq!(output, vec![BitState::On]);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 mod bit;
 mod nand;
 mod not;
+mod and;
 
 use digital_component::BitState;
 
 fn main() {
     let _ = bit::bit;
     let _ = not::not;
+    let _ = and::and;
 }
