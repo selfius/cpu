ce327dc8a573bbe93a8ab77f54e1849808fb2348
Andrei Sviridov <self.polaris@gmail.com>
2025-01-01 12:10:36 -0500

    Introduce AND gate cascading for the cases when we need n-input AND gates

--- src/and.rs
+++ src/and.rs
@@ -1,6 +1,9 @@
 use crate::nand::nand;
 use crate::not::not;
-use digital_component::{ComponentLogic, ComponentLogicFactory};
+use digital_component::{
+    ComponentInput, ComponentLogic, ComponentLogicFactory, ComponentOutput, DigitalComponent,
+    Graph, NodeKind,
+};
 use parser::parse;
 use std::collections::HashMap;
 
@@ -21,30 +24,107 @@ pub fn and() -> Box<ComponentLogic> {
     .finalize()
 }
 
+/// Cascade n - 1 AND gates to have an n input AND gate.
+/// For example for n = 3 an equivalent to the following circuit will be generated:
+///     ┏━━━┓
+///    ─┨and┠┐
+///    ─┨   ┃│ ┏━━━┓
+///     ┗━━━┛└─┨and┠─
+///    ────────┨   ┃
+///            ┗━━━┛
+///
+pub fn cascade_and(n: usize) -> Box<ComponentLogicFactory> {
+    Box::new(move || -> Box<ComponentLogic> {
+        let mut graph = Graph::default();
+        let and_gates = (0..n - 1)
+            .map(|_| graph.add_component(DigitalComponent::new(2, 1, and())))
+            .collect::<Vec<_>>();
+
+        let outer_inputs = (0..n)
+            .map(|input_idx| graph.add_node(NodeKind::Input(input_idx)))
+            .collect::<Vec<_>>();
+
+        let outer_output = graph.add_node(NodeKind::Output(0));
+
+        let and_gates_inputs = and_gates
+            .iter()
+            .map(|and_gate| {
+                (
+                    graph.add_node(NodeKind::ComponentInput(ComponentInput::new(*and_gate, 0))),
+                    graph.add_node(NodeKind::ComponentInput(ComponentInput::new(*and_gate, 1))),
+                )
+            })
+            .collect::<Vec<_>>();
+
+        let and_gates_outputs = and_gates
+            .iter()
+            .map(|and_gate| {
+                graph.add_node(NodeKind::ComponentOutput(ComponentOutput::new(
+                    *and_gate, 0,
+                )))
+            })
+            .collect::<Vec<_>>();
+
+        graph.add_edge(&outer_inputs[0], &and_gates_inputs[0].0);
+
+        for (outer_input, and_gate_inputs) in outer_inputs[1..n].iter().zip(and_gates_inputs.iter())
+        {
+            graph.add_edge(outer_input, &and_gate_inputs.1);
+        }
+
+        for (and_gate_output, next_and_gate_inputs) in and_gates_outputs
+            .iter()
+            .zip(and_gates_inputs.iter().skip(1))
+        {
+            graph.add_edge(and_gate_output, &next_and_gate_inputs.0);
+        }
+
+        graph.add_edge(&outer_output, and_gates_outputs.iter().last().unwrap());
+
+        graph.finalize()
+    })
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    use digital_component::*;
+    use digital_component::BitState::*;
 
     #[test]
     fn ands() {
         let mut and_gate = and();
 
-        let mut output = vec![BitState::Undefined];
+        let mut output = vec![Undefined];
 
-        and_gate(&[BitState::Off, BitState::Undefined], &mut output);
-        assert_eq!(output, vec![BitState::Off]);
+        and_gate(&[Off, Undefined], &mut output);
+        assert_eq!(output, vec![Off]);
 
-        and_gate(&[BitState::Off, BitState::Off], &mut output);
-        assert_eq!(output, vec![BitState::Off]);
+        and_gate(&[Off, Off], &mut output);
+        assert_eq!(output, vec![Off]);
 
-        and_gate(&[BitState::On, BitState::Off], &mut output);
-        assert_eq!(output, vec![BitState::Off]);
+        and_gate(&[On, Off], &mut output);
+        assert_eq!(output, vec![Off]);
 
-        and_gate(&[BitState::On, BitState::Off], &mut output);
-        assert_eq!(output, vec![BitState::Off]);
+        and_gate(&[On, Off], &mut output);
+        assert_eq!(output, vec![Off]);
 
-        and_gate(&[BitState::On, BitState::On], &mut output);
-        assert_eq!(output, vec![BitState::On]);
+        and_gate(&[On, On], &mut output);
+        assert_eq!(output, vec![On]);
+    }
+
+    #[test]
+    fn cascades_and_gates() {
+        let mut and_gate = cascade_and(3)();
+
+        let mut output = vec![Undefined];
+
+        and_gate(&[Off, Off, Undefined], &mut output);
+        assert_eq!(output, vec![Off]);
+
+        and_gate(&[Off, On, Undefined], &mut output);
+        assert_eq!(output, vec![Off]);
+
+        and_gate(&[On, On, On], &mut output);
+        assert_eq!(output, vec![On]);
     }
 }
